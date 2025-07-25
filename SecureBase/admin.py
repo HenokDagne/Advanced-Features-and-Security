7c6af3d921277a467c9c3f5a910accfa1415678e
henok dagne <henokdagne19@gmail.com>
2025-07-25 22:23:26 +0300

    registering CustomUser in admin site

--- SecureBase/admin.py
+++ SecureBase/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, PremiumUser
 
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id','username', 'email', 'first_name', 'last_name', 'bio', 'birth_date', 'profile_picture', 'is_staff', 'is_active')
+    search_field = ('email', 'first_name', 'last_name', 'username')
+    list_editable = ('username', 'email', 'first_name', 'last_name', 'bio', 'birth_date', 'profile_picture','is_active', 'is_staff')
+    list_filter = ('is_staff', 'is_active', 'is_superuser', 'email')
+
+    #Define the fieldsets for adding and changing users in the admin
+    # this structure is similar to the default UserAdmin
+    # but we add our custom fields
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('bio', 'birth_date', 'profile_picture')}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            'fields': ('email',)
+        }),
+    )
+
+     
+    
